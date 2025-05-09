5f5f0f772a579fd254715fb3b1437c86e58a1cd9
Marek <marek.sloser@gmail.com>
2025-05-09 14:28:37 +0200

    I create a new program for test..

---
+++ test.py
@@ -0,0 +1,10 @@
+pi          = 3.14
+polomer     = float(input("Zadej poloměr kruhu (cm):"))
+
+obvod       = 2 * pi * polomer
+obsah       = pi * polomer ** 2
+
+
+print(f"Obvod zadaného kruhu je: {obvod}")
+print(f"Obsah kruhu je: {obsah}")
+
